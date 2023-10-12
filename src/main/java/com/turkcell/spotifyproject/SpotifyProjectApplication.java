f552c292f7ac066b59ea42984b05ebf220567bab
emrealkan884 <Hasan.alkan884@gmail.com>
2023-10-12 13:15:12 +0300

    Spotify Project With Spring

---
+++ src/main/java/com/turkcell/spotifyproject/SpotifyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.turkcell.spotifyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyProjectApplication.class, args);
+	}
+
+}
