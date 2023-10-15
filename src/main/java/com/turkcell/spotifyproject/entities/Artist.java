45c251c0c9acefadbc6fd3896ad80dc7a109992a
Nurdan Emin <nurdanemin0@gmail.com>
2023-10-15 15:48:19 +0300

    Add music entity

--- src/main/java/com/turkcell/spotifyproject/entities/Artist.java
+++ src/main/java/com/turkcell/spotifyproject/entities/Artist.java
@@ -1,4 +1,24 @@
 package com.turkcell.spotifyproject.entities;
 
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "artists")
 public class Artist {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int id;
+
+  private String name;
+  @OneToMany private Set<Music> musics = new HashSet<>();
 }
--- src/main/java/com/turkcell/spotifyproject/entities/Music.java
+++ src/main/java/com/turkcell/spotifyproject/entities/Music.java
@@ -1,4 +1,22 @@
 package com.turkcell.spotifyproject.entities;
 
+import jakarta.persistence.*;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "musics")
 public class Music {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private UUID id;
+
+  private String name;
 }
