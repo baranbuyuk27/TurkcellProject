87f7d76fe9298d751e285de0ce4ba5dd112587ed
Nurdan Emin <nurdanemin0@gmail.com>
2023-10-15 23:31:32 +0300

    Add  album entity and repository

--- src/main/java/com/turkcell/spotifyproject/entities/Album.java
+++ src/main/java/com/turkcell/spotifyproject/entities/Album.java
@@ -1,4 +1,33 @@
 package com.turkcell.spotifyproject.entities;
 
+import jakarta.persistence.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "albums")
 public class Album {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private UUID id;
+
+  private String name;
+  private LocalDate releaseDate;
+  private long numberOfLikes;
+
+  @ManyToOne
+  @JoinColumn(name = "artist id")
+  private Artist artist;
+
+  @OneToMany(mappedBy = "album")
+  private List<Music> musics;
 }
