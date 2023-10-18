e3752634c809da8fb1c7f82f51cac49436b8674a
Nurdan Emin <nurdanemin0@gmail.com>
2023-10-18 16:03:41 +0300

    Add album dtos service and controller

--- src/main/java/com/turkcell/spotifyproject/business/concretes/AlbumServiceImp.java
+++ src/main/java/com/turkcell/spotifyproject/business/concretes/AlbumServiceImp.java
@@ -1,3 +1,39 @@
 package com.turkcell.spotifyproject.business.concretes;
 
-public class AlbumServiceImp {}
+import com.turkcell.spotifyproject.business.abstracts.AlbumService;
+import com.turkcell.spotifyproject.business.dto.CreateAlbumRequest;
+import com.turkcell.spotifyproject.business.dto.CreateAlbumResponse;
+import com.turkcell.spotifyproject.business.dto.GetAlbumResponse;
+import com.turkcell.spotifyproject.entities.Album;
+import com.turkcell.spotifyproject.repositories.AlbumRepository;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AlbumServiceImp implements AlbumService {
+  private final AlbumRepository albumRepository;
+  private ModelMapper mapper;
+
+  @Override
+  public GetAlbumResponse getById(UUID id) {
+    Album album = albumRepository.findById(id).orElseThrow();
+    return mapper.map(album, GetAlbumResponse.class);
+  }
+
+  @Override
+  public List<GetAlbumResponse> getAll() {
+    albumRepository.findAll();
+  }
+
+  @Override
+  public CreateAlbumResponse createAlbum(CreateAlbumRequest albumRequest) {
+    return null;
+  }
+
+  @Override
+  public void delete(UUID id) {}
+}
---
+++ src/main/java/com/turkcell/spotifyproject/business/dto/GetAlbumResponse.java
@@ -0,0 +1,23 @@
+package com.turkcell.spotifyproject.business.dto;
+
+import com.turkcell.spotifyproject.entities.Music;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class GetAlbumResponse {
+  private UUID id;
+  private String name;
+  private LocalDate releaseDate;
+  private long numberOfLikes;
+  private String artistName;
+  private List<Music> musics;
+}
