0adb37f8f595ee9d9a8571d8e4c49eb5d9e4944b
emrealkan884 <Hasan.alkan884@gmail.com>
2023-10-25 22:20:38 +0300

    Added Basic Validations

--- src/main/java/com/turkcell/spotifyproject/business/dto/music/MusicForAddDto.java
+++ src/main/java/com/turkcell/spotifyproject/business/dto/music/MusicForAddDto.java
@@ -1,10 +1,20 @@
 package com.turkcell.spotifyproject.business.dto.music;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class MusicForAddDto {
+  @NotBlank(message = "Sarki ismi bos birakilamaz.")
+  @Size(min = 3, max = 40, message = "Sarki ismi 3-40 hane arasinda olmalidir.")
   private String name;
+
   private String link;
+
+  @NotNull(message = "Sure bos birakilamaz")
+  @Positive(message = "Sure pozitif olmalidir.")
   private float duration;
 }
